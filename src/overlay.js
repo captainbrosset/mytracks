a1b7d14c1c962a6f3e3162ac33ce342d7a8c8cd7
captainbrosset <captainbrosset@users.noreply.github.com>
2021-08-09 09:08:42 +0000

    deploy: 3c7e68b6706fb12b058fd968c3598f5f71082c91

--- src/overlay.js
+++
@@ -1,9 +0,0 @@
-import { debounce } from "./utils";
-
-if ('windowControlsOverlay' in navigator) {
-    document.documentElement.classList.toggle('has-control-overlay', navigator.windowControlsOverlay.visible);
-
-    navigator.windowControlsOverlay.ongeometrychange = debounce((e) => {
-        document.documentElement.classList.toggle('has-control-overlay', navigator.windowControlsOverlay.visible);
-    }, 250);
-}
