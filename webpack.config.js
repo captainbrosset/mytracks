2866eb3dea0480655b644fe4a42a0eeebc246ab3
Patrick Brosset <patrickbrosset@gmail.com>
2021-08-09 11:30:50 +0200

    Moving all website files to public

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
 };
